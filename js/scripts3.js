ae857f89f3c54edce25fb1fb3f2631c9b7989480
scottorr-4 <orrs@oregonstate.edu>
2025-02-22 17:54:12 -0800

    Update scripts3.js

--- js/scripts3.js
+++ js/scripts3.js
@@ -69,7 +69,7 @@ function createMap2() {
             opacity: 1,
             color: 'white',
             dashArray: '3',
-            fillOpacity: 0.7
+            fillOpacity: 0
         };
     }
 
